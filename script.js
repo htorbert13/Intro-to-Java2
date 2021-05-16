fb3085b80c718b74d2fe80ff3f77175928af870b
htorbert13 <83992822+htorbert13@users.noreply.github.com>
2021-05-16 14:29:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = "Hope";
+var color = "red"
+var intro = "Hello, my name is "+name;
+var intro2 = "My favorite color is "+color;
+
+document.querySelector(".words").innerHTML = intro;
+document.querySelector(".red").innerHTML = intro2;
